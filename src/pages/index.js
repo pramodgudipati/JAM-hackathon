be6ec3fcd7d8d1cd45c5a30fc0c143c0af1f4e2c
Pramod Kumar Gudipati <gudipati@adobe.com>
2018-10-07 12:52:30 -0700

    Initial code

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi folks</h1>
+    <p>Welcome to hackathon demo site.</p>
+    <p>Excited to be part of this camp.</p>
+  </Layout>
+)
+
+export default IndexPage
